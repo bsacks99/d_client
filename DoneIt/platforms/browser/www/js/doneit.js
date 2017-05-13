29bf1941aa5bc302c698545df577d64d15b113b4
Brad Sacks <bradsacks99@gmail.com>
2017-05-12 22:01:11 -0700

    Updated Framework7 to newest version and added sign_up.html page

--- DoneIt/platforms/browser/www/js/doneit.js
+++ DoneIt/platforms/browser/www/js/doneit.js
@@ -1,12 +1,12 @@
 // Initialize app
-var myApp = new Framework7();
+var DoneIt = new Framework7();
 
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
+var mainView = DoneIt.addView('.view-main', {
     // Because we want to use dynamic navbar, we need to enable it for this view:
     dynamicNavbar: true
 });
@@ -26,9 +26,16 @@ $$(document).on('deviceready', function() {
 // Now we need to run the code that will be executed only for About page.
 
 // Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
+DoneIt.onPageInit('about', function (page) {
 
+
+})
+
+DoneIt.onPageInit('sign_up', function (page) {
+    $$('.sign-up-submit').on('click', function(){
+        var formData = DoneIt.formToData('#sign_up');
+        console.log(formData)
+    }); 
 })
 
 // Option 2. Using one 'pageInit' event handler for all pages:
@@ -38,12 +45,12 @@ $$(document).on('pageInit', function (e) {
 
     if (page.name === 'about') {
         // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+        DoneIt.alert('Here comes About page');
     }
 })
 
 // Option 2. Using live 'pageInit' event handlers for each page
 $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
+    DoneIt.alert('Here comes About page');
 })
